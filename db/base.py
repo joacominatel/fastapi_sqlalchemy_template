285e7aa34ceb47b2168d66f59618497d7fc9f4ae
Joaquin minatel <86282635+joacominatel@users.noreply.github.com>
2025-11-07 12:52:21 -0300

    Add base model standard

---
+++ db/base.py
@@ -0,0 +1,24 @@
+import re
+from sqlalchemy.orm import DeclarativeBase, declared_attr
+from sqlalchemy import MetaData
+
+metadata = MetaData(
+    naming_convention={
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+)
+
+def camel_to_snake(name: str) -> str:
+    s1 = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
+    return re.sub("([a-z0-9])([A-Z])", r"\1_\2", s1).lower()
+
+class Base(DeclarativeBase):
+    metadata = metadata
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return camel_to_snake(cls.__name__)
